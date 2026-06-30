1c13f43f35ddf15571d2d2c158935dc4edd22f5a
hasnatsakil <abulhasnatsakil.cu@gmail.com>
2026-07-01 01:40:50 +0800

    diagnostic: add specific /test-rewrite rewrite to test if rewrites work

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,8 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import { vercel } from "vite-plugin-vercel/vite"
 export default defineConfig({ 
-  plugins: [react(), vercel({ 
-    rewrites: [{ source: "/(.*)", destination: "/index.html" }] 
-  })], 
+  plugins: [react()], 
   resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
   server: {
     proxy: {
