dcadbf8015df84fb96b371514b6f01ebe8eb3c9c
hasnatsakil <abulhasnatsakil.cu@gmail.com>
2026-07-01 00:33:58 +0800

    fix vite-plugin-vercel import: vercelPlugin -> vercel

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,9 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import { vercelPlugin } from "vite-plugin-vercel"
+import { vercel } from "vite-plugin-vercel/vite"
 export default defineConfig({ 
-  plugins: [react(), vercelPlugin({ 
+  plugins: [react(), vercel({ 
     rewrites: [{ source: "/(.*)", destination: "/index.html" }] 
   })], 
   resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
